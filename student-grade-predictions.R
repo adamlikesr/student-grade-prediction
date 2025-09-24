4321941bf0afd915ce249ae17bd11ea559644300
Adam Estrellado <adamestrellado@Adams-MacBook-Air.local>
2025-09-24 11:28:54 -0700

    feats:
    - Changed outupt to display lower and upper bound of predicted results
    
    - Calculate predicted grade as a percentage

--- student-grade-predictions.R
+++ student-grade-predictions.R
@@ -78,6 +78,7 @@ rmse <- sqrt(mse)
 # 7. Build Function
 #-----------------------------
 
+# Create function to predict final_grade from user inputs
 inputPredict <- 
   function(user_classes_failed, user_hours_studied, user_absences, 
                          user_travel_time, user_free_time, user_weekend_drinking, 
@@ -102,4 +103,75 @@ inputPredict <-
     return(user_prediction)
 }
 
+
+predictGrade <- function() {
+  # Greet user
+  cat("* Thank you for using the GradePredictor!     *\n* You can stop the program by typing \"stop\"   *\n* Answer the questions truthfully, and enjoy! *")
+  
+  # Function to check validity of input
+  isValid <- function(user_prompt, min_val, max_val) {
+    repeat {
+      input = readline(prompt = user_prompt)
+      
+      # Check if user wants to stop, return NULL to stopAndStore
+      if (tolower(input) == "stop") {
+        return(NULL)
+      }
+      
+      # Checks if input is in range 
+      num <-  suppressWarnings(as.numeric(input))
+      if(!is.na(num) && num >= min_val && num <= max_val) {
+        return(num)
+      } 
+      
+      # Displays error message
+      else {
+        cat("Invalid input, please enter a number between ", min_val, " and ", max_val, ".\n")
+      }
+    }
+  }
+  
+  # Helper function to store inputs, and stop program at user command
+  stopAndStore <- function(user_prompt, min_val, max_val) {
+    x <- isValid(user_prompt, min_val, max_val)
+    
+    # Check if user wants to stop
+    if(is.null(x)) {
+      cat("Terminating program. Have a nice day!")
+      stop()
+    }
+    return(x)
+  }
+  
+  
+  # Ask user questions, store into user variables
+  user_classes_failed <- stopAndStore("How many classes have you failed the past semester? If more than 3, enter 4: ", 0, 4)
+  
+  user_hours_studied <- stopAndStore("How much do you study a week on a scale from 1 to 4? 1 being not much (less than two hours a week), 4 being very frequently (more than 10 hours a week): ", 1, 4)
+  
+  user_absences <- stopAndStore("In that past 90 days, how many times have you missed school: ", 0, 90)
+  
+  user_travel_time <- stopAndStore("On a scale from 1 to 4, how long is your commute? 1 being very short (less than 15 minutes), 4 being very long (more than 1 hour): ", 1, 4)
+  
+  user_free_time <- stopAndStore("On a scale from 1 to 5, how much free time do you have after school? 1 being very little, 5 being very much: ", 1, 5)
+  
+  user_workday_drinking <- stopAndStore("On a scale from 1 to 5, how often do you drink during school days? 1 being little to never, 5 being very frequently: ", 1, 5)
+  
+  user_weekend_drinking <- stopAndStore("On a scale from 1 to 5, how often do you drink on the weekends? 1 being little to never, 5 being very frequently: ", 1, 5)
+  
+  user_health <- stopAndStore("On a scale from 1 to 5, how is your health status? 1 being very bad, 5 being extremley healthy: ", 1, 5)
+
+  # Predict final grade
+  output_grade <- inputPredict(user_classes_failed, user_hours_studied, user_absences,
+                               user_travel_time, user_free_time, user_workday_drinking,
+                               user_weekend_drinking, user_health)
+  
+  # Calculate grade as %, find lower and upper error bound of result
+  output_grade_perc <- (output_grade / 20)
+  output_grade_perc_lower_bound <- (output_grade_perc - (rmse / 20))
+  output_grade_perc_upper_bound <- (output_grade_perc + (rmse / 20))
+  
+  # Output results
+  cat("Using your data, the model predicts that your grade will likely be between", output_grade_perc_lower_bound, "% and ", output_grade_perc_upper_bound, "%.")
+}
 
